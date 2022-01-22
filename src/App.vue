3d0ece12dc3f0af59a55386abda4567e65df1660
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-22 11:53:33 -0300

    transitando components dinamicos

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,29 @@
             <option value="slide">Slide</option>
           </select>
        </div>
+      <div class="form-group">
+          <label>Componente:</label>
+            <select name="mensagem" id="selectMensagens" class="form-control" v-model="componentSelecionado">
+              <option value="AppHome">Home</option>
+              <option value="AppSobre">Sobre</option>
+            </select>
+        </div>
 
-        <div class="form-group">
+        <transition :name="animacaoSelecionada" mode="out-in">
+          <Component :is="componentSelecionado"></Component>
+        </transition>
+        <!-- <div class="form-group">
           <label>Mensagem:</label>
             <select name="mensagem" id="selectMensagens" class="form-control" v-model="alertaAtual">
               <option value="info">Informação</option>
               <option value="warning">Alerta</option>
               <option value="success">Sucesso</option>
             </select>
-        </div>
+        </div> -->
 
-      <transition :name="animacaoSelecionada" mode="out-in">
+      <!-- <transition :name="animacaoSelecionada" mode="out-in">
         <div :class="classeDeAlerta" :key="alertaAtual"> {{ mensagemDinamica }}</div>
-      </transition>
+      </transition> -->
 
       <!-- <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button> -->
       <!-- 2 Elementos diferentes dentro do transition com if e else funcionam, mas com 2 divs ou seja 2 elementos iguais, o vue precisa de uma key pra saber q sao 2 elementos diferentes-->
@@ -88,86 +98,92 @@
         <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
       </transition> -->
     </div>
-
   </div>
 </template>
 
 <script>
 
 export default {
-   data(){
-     return{
-       mostrar: true,
-       animacaoSelecionada: 'fade',
-       alertaAtual: 'info'
-     }
-   },
-   computed:{
-     classeDeAlerta(){
-       return{
-         alert: true,
-         [`alert-${this.alertaAtual}`]: true
-       }
-     },
-     mensagemDinamica(){
+
+  components:{
+    AppHome: () => import('./components/Home.vue'),
+    AppSobre: () => import('./components/Sobre.vue')
+  },
+
+  data(){
+    return{
+      mostrar: true,
+      animacaoSelecionada: 'fade',
+      alertaAtual: 'info',
+      componentSelecionado: 'AppHome'
+    }
+  },
+  computed:{
+    classeDeAlerta(){
+      return{
+        alert: true,
+        [`alert-${this.alertaAtual}`]: true
+      }
+    },
+    mensagemDinamica(){
         if(this.alertaAtual ==='info') return 'Animações no Vue (Informação)'
         else if( this.alertaAtual ==='warning') return 'Animações no Vue (Alerta)'
         else return'Animações no Vue (Sucesso)'
-     }
-   },
-   methods:{
-     beforeEnter(el){
-      console.log("BeforeEnter ")
-      el.style.opacity = 0
-     },
-     enter(el, done){
-      console.log("enter "+el)
+    }
+  },
+  methods:{
+    beforeEnter(el){
+    console.log("BeforeEnter ")
+    el.style.opacity = 0
+    },
+    enter(el, done){
+    console.log("enter "+el)
+    let contagem = 0
+    const intervalo = setInterval(()=>{
+      el.style.opacity = + el.style.opacity + 0.1
+      contagem++
+      if(contagem > 10 ){
+        clearInterval(intervalo)
+        done()
+      }
+    }, 150 )
+
+    },
+    afterEnter(el){
+      console.log("afterEnter "+el)
+    },
+    enterCancelled(el){
+      console.log("enterCancelled "+el)
+    },
+
+    beforeLeave(el){
+      console.log("beforeLeave ")
+      el.style.transition = 'width 0.5s'
+      el.style.overflow = 'hidden'
+      el.style.whiteSpace = 'nowrap'
+    },
+    leave(el, done){
+      console.log("Leave ")
       let contagem = 0
+      const tamanho = el.offsetWidth
+
       const intervalo = setInterval(()=>{
-        el.style.opacity = + el.style.opacity + 0.1
+        el.style.width = (tamanho * (1 - (contagem / 10))) + 'px'
         contagem++
-        if(contagem > 10 ){
+
+        if( contagem > 10 ){
           clearInterval(intervalo)
           done()
         }
       }, 150 )
-
-     },
-     afterEnter(el){
-       console.log("afterEnter "+el)
-     },
-     enterCancelled(el){
-       console.log("enterCancelled "+el)
-     },
-
-      beforeLeave(el){
-        console.log("beforeLeave ")
-        el.style.transition = 'width 0.5s'
-        el.style.overflow = 'hidden'
-        el.style.whiteSpace = 'nowrap'
-      },
-      leave(el, done){
-        console.log("Leave ")
-        let contagem = 0
-        const tamanho = el.offsetWidth
-
-        const intervalo = setInterval(()=>{
-          el.style.width = (tamanho * (1 - (contagem / 10))) + 'px'
-          contagem++
-
-          if( contagem > 10 ){
-            clearInterval(intervalo)
-            done()
-          }
-        }, 150 )
-      },
-      afterLeave(el){
-        console.log("afterLeave "+el)
-      },
-      leaveCancelled(el){
-        console.log("LeaveCancelled "+el)
-      },
-   }
+    },
+    afterLeave(el){
+      console.log("afterLeave "+el)
+    },
+    leaveCancelled(el){
+      console.log("LeaveCancelled "+el)
+    },
+  }
 }
 </script>
 
